dba78f5020a213c9f56d7721916e3331c751d046
Card Games <freecardgames99@gmail.com>
2024-07-06 14:07:44 +0200

    Update and rename build_desura_vis.bat to build_cardgames_vis.bat

--- build_cardgames_vis.bat
+++ build_cardgames_vis.bat
@@ -4,5 +4,5 @@ IF NOT EXIST build_vis (
 
 cd build_vis
 cmake .. -DCMAKE_INSTALL_PREFIX=../install_vis -G "Visual Studio 12" -T "v120_xp" -DBUILD_CEF=OFF -DDEBUG=ON -DBUILD_TESTS=ON -DBUILD_TESTING=ON -DDEBUG_V8=ON
-start Desura.sln
+start CardGames.sln
 cd ..
